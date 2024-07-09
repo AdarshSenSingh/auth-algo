d6fbf9d6572472dabf68e6c98e27fdd9acee90a2
AdarshSenSingh <adarshsensingh@gmail.com>
2024-07-09 14:51:45 +0530

    verdict

---
+++ compiler/backend/Verdict.js
@@ -0,0 +1,7 @@
+// here verdict code will be there
+// steps need to do 
+//1- need to fetch the test cases from the crud backend 
+//2- need to fetch the output from the output file in compiler backend
+// 3- need to verify whether the output is correct or not
+//4- need to display the user submissions on the result page
+// 5- donot show the test cases in network tab ie update the crud backend to.
